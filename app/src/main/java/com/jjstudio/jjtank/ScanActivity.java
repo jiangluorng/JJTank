bd66add193bf8ca41e51ea12120ffd05e870261d
Charlie Jiang <yixiu.jiang@gmail.com>
2018-08-19 11:46:09 +1000

    scan qr to connect to tank

--- app/src/main/java/com/jjstudio/jjtank/ScanActivity.java
+++ app/src/main/java/com/jjstudio/jjtank/ScanActivity.java
@@ -1,31 +1,50 @@
 package com.jjstudio.jjtank;
 
+import android.content.Intent;
 import android.graphics.PointF;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
 
-public class ScanActivity extends AppCompatActivity implements QRCodeReaderView.OnQRCodeReadListener {
+public class ScanActivity extends AppCompatActivity implements QRCodeReaderView.OnQRCodeReadListener, View.OnClickListener {
 
     private QRCodeReaderView qrCodeReaderView;
 
     private TextView tankDeviceAddress;
 
+    private String tankUUID;
+
+    private Button connectButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
+        tankUUID ="";
         qrCodeReaderView = findViewById(R.id.qrdecoderview);
+        connectButton = findViewById(R.id.connectButton);
         tankDeviceAddress= findViewById(R.id.tankDeviceAddress);
+        connectButton.setOnClickListener(this);
         initQRScanner();
     }
 
     @Override
+    public void onClick(View view) {
+        final Intent intent = new Intent(ScanActivity.this, MainActivity.class);
+        intent.putExtra(MainActivity.EXTRAS_DEVICE_ADDRESS, tankUUID);
+        startActivity(intent);
+    }
+
+    @Override
     public void onQRCodeRead(String text, PointF[] points) {
-        tankDeviceAddress.setText(text);
+        tankUUID = text;
+        connectButton.setVisibility(View.VISIBLE);
+        tankDeviceAddress.setText("Found tank -> " +text);
         Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
     }
 
